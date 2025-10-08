d1834415c98545a709c2997a22000de6a04ab655
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2025-10-08 10:07:57 -0500

    Update random-number.py
    Final random number update

--- random-number.py
+++ random-number.py
@@ -1,51 +1,28 @@
 import random
 
-#initializing win variables
-computerWin = 0
-playerWin = 0
-#setting up parameters for options and choice randomization
-optionsList = ['rock', 'paper', 'scissors']
+randNum = random.randint(1, 1000);
+#print(randNum)
 
-print("Welcome to Rock, Paper, Scissors!")
-print("Best of 3 wins.\n")
+guess = -1
 
-#setting up while loop to run to best of 3
-while playerWin < 3 and computerWin < 3:
-  #initializing randomChoice 
-  randomChoice = random.choice(optionsList)
-  #gameplay while loop
-  while True:
-      #user inputs choice
-      userInput = input("What do you choose? Rock, Paper, or Scissors? ").strip().lower()
-      if userInput in optionsList:
-        playerChoice = userInput
-        break
-      else:
-        print("That is not a valid choice. Try again.")
-        continue
+print("It's guessing game time!")
+print("To start, please enter a\nnumber into the input box.\n")
+print("The module will tell you\nif you are close to the\nnumber or not.\n")
+print("The number is between\n1 and 1000.\n")
+print("Let's get started!\n")
 
-  #choice comparisons and outcomes
-  if playerChoice == randomChoice:
-    print("You chose: " + playerChoice)
-    print("Computer chose: " + randomChoice)
-    print("That's a tie!")
-  elif (playerChoice == "rock" and randomChoice == "scissors") or \
-         (playerChoice == "paper" and randomChoice == "rock") or \
-         (playerChoice == "scissors" and randomChoice == "paper"):
-    print("You chose: " + playerChoice)
-    print("Computer chose: " + randomChoice)
-    print("Player +1")
-    playerWin += 1
-  else:
-    print("Computer +1")
-    computerWin += 1
-  #game score output
-  print(f"\nSCORE: Player {playerWin} - Computer {computerWin}")
-#displays who wins after reaching 3 points
-if playerWin == 3:
-  print("-" * 20)
-  print("Player wins!")
-elif computerWin == 3:
-  print("-" * 20)
-  print("Computer wins!")
+while guess != randNum:
+  try:
+    user_input = input("Please enter your guess: ")
+    guess = int(user_input)
+  except ValueError:
+    print("Must be a valid number. Please try again.")
+    continue
 
+  if(guess < randNum):
+    print("Too low. Try again!\n")
+  elif (guess > randNum):
+    print("Too high. Try again!\n")
+  elif (guess == randNum):
+    print(f"You got it! The number was {randNum}.")
+    break
