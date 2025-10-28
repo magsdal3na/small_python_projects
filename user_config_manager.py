a463f81a5bb583e1d67e0f47aff67b7f27f46d09
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2025-10-28 11:08:48 -0500

    Create user_config_manager.py
    very very beginning of layout

---
+++ user_config_manager.py
@@ -0,0 +1,20 @@
+def add_setting(dict, tuple):
+    og_settings_dict = {}
+    lower_settings_dict = {key.lower(): k for v, k in og_settings_dict.keys()}
+    add_setting.lower(value())
+    if key in add_setting():
+        print(f"Setting '{key}' already exists! Cannot add a new setting with this name.")
+    else:
+        add_setting.append()
+        print(f"Setting '{key}' added with value '{value}' successfully!")
+
+def update_setting(dict, tuple):
+    pass
+
+def delete_setting(dict, key):
+    pass
+
+def view_settings(dict):
+    pass
+
+test_settings = {}
